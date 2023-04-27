61fdff1350c73b9ae992a3127c71f5518e6c0ed0
Bluzzi <camilledugas.pro@gmail.com>
2023-04-27 18:47:09 +0200

    feat: add new slash commands
    Add new slash commands for avatar, ping and say. These commands allow users to get their own or other users' avatars, reply with "Pong!" and make the bot say a message.

--- src/configs/guild.ts
+++ src/configs/guild.ts
@@ -1,3 +1,7 @@
+// Juste un exemple de config, que tu peux importer n'importe ou,
+// pour les informations secret (genre un token de bot), ça n'a pas sa place dans ce genre de config mais plutôt dans un .env
+// j'en ai créé un à la racine de ton projet.
+
 export const guild = {
   id: "732251741999071303"
 }
